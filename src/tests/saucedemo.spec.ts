136bba5886ee9af62c774f2cf23d5b46661c538d
sharif-jobayed <sharif.jobayed@gmail.com>
2026-05-25 16:08:24 +0600

    Change branch name

--- src/tests/saucedemo.spec.ts
+++
@@ -1,9 +0,0 @@
-import { test, expect } from "@playwright/test";
-import { LoginPage } from "../pages/LoginPage";
-
-test(`login and verify inventory`, async ({ page }) => {
-	const loginPage = new LoginPage(page);
-	await loginPage.goto(``);
-	await loginPage.login(`sandard_user`, `secret_sauce`);
-	await expect(page).toHaveURL(/inventory/);
-});
